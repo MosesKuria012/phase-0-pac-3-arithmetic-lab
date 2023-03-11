67328b53497412a305f17612eafbd0f7f68d06bf
Moses <t.content012@gmail.com>
2023-03-11 18:20:15 +0300

    mot fudje

---
+++ index.js
@@ -0,0 +1,52 @@
+function add(num1, num2){
+    return num1+num2
+    }
+    console.log(add(1, 3))
+
+    function subtract(num1, num2){
+        return num1-num2
+        }
+        console.log(subtract(5, 3))
+
+        function multiply(num1, num2){
+            return num1*num2
+            }
+            console.log(multiply(1, 3))
+
+            function divide(a, b){
+                return a/b
+                }
+                console.log(divide(3, 3))
+
+
+                function increment(num) {
+                    return num ++;
+                  }
+                  
+                  function decrement(num) {
+                    return num --;
+                  }
+                  function increment(num) {
+  return num + 1
+  }
+  function decrement(num) {
+    return num - 1
+    }
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+let n = "123";
+let parsedInt = makeInt(m);
+
+console.log(parsedInt); // Output: 123
+
+function preserveDecimal(m) {
+  return parseFloat(m, 10);
+}
+
+let m = "123.0909";
+let parsedfloat = makpreserveDecimaleInt(n);
+
+console.log(parsedFloat); // Output: 123
